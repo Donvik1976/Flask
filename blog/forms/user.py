ee4c471ce4c2d1f1a99771d57ac44390eb08826e
Donvik1976 <98390338+Donvik1976@users.noreply.github.com>
2023-05-01 15:29:24 +0600

    lesson_5 Lesson 5 ready

---
+++ blog/forms/user.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, validators, PasswordField, SubmitField
+
+
+class UserBaseForm(FlaskForm):
+    username = StringField('Username', [validators.DataRequired()])
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    email = StringField('E-mail', [validators.DataRequired(), validators.Email()])
+
+
+class UserRegisterForm(UserBaseForm):
+    password = PasswordField("Password", [
+        validators.DataRequired(),
+        validators.EqualTo('confirm_password', message='Field must be equal to password')
+    ])
+    confirm_password = PasswordField("Confirm Password", [validators.DataRequired()])
+    submit = SubmitField("Register")
+
+
+class UserLoginForm(FlaskForm):
+    email = StringField('E-mail', [validators.DataRequired(), validators.Email()])
+    password = PasswordField("Password", [validators.DataRequired()])
+    submit = SubmitField("Login")
--- blog/models.py
+++ blog/models.py
@@ -9,15 +9,19 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255))
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
-    def __init__(self, username, email, password, is_admin):
+    def __init__(self, username, first_name, last_name, email, password):
         self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
-        self.is_admin = is_admin
+
 
 
 
