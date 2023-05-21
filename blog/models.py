cb875c54a7a00f2af0c19947db826e3b67b46307
Donvik1976 <vdonets76@mail.ru>
2023-05-21 22:21:19 +0600

    lesson_8 All tasks completed

--- blog/models.py
+++ blog/models.py
@@ -35,6 +35,9 @@ class User(db.Model, UserMixin):
         self.email = email
         self.password = password
 
+    def __str__(self):
+        return f'{self.user.email} ({self.user.id})'
+
 
 class Author(db.Model):
     __tablename__ = 'authors'
@@ -44,6 +47,9 @@ class Author(db.Model):
 
     user = relationship('User', back_populates='author')
     articles = relationship('Article', back_populates='author')
+
+    def __str__(self):
+        return self.user.email
 
 
 class Article(db.Model):
@@ -59,6 +65,9 @@ class Article(db.Model):
     author = relationship('Author', back_populates='articles')
     tags = relationship('Tag', secondary=article_tag_associations_table, back_populates='articles')
 
+    def __str__(self):
+        return self.title
+
 
 class Tag(db.Model):
     __tablename__ = "tags"
@@ -67,6 +76,9 @@ class Tag(db.Model):
     name = db.Column(db.String(255), nullable=False)
 
     articles = relationship('Article', secondary=article_tag_associations_table, back_populates='tags')
+
+    def __str__(self):
+        return self.name
 
 
 
